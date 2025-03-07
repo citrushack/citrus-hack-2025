7a7a96e527499d3b56ac7bc2c0bcd3836c133981
Howard Zhu <67174263+Howard-Z@users.noreply.github.com>
2025-03-06 18:54:21 -0800

    updated the config file

--- src/data/config.ts
+++ src/data/config.ts
@@ -16,20 +16,20 @@ type config = {
 };
 
 const data: config = {
-  name: "HACKATHON",
-  short_name: "Hackathon Site",
-  email: "contact.acmucr@gmail.com",
-  description: "DESCRIPTION",
-  length: 24,
-  date: new Date("2024-04-12T13:20:00"),
-  end: new Date("2024-04-12T13:20:00"),
+  name: "Citrus Hack",
+  short_name: "Citrus Hack Site",
+  email: "citrushack@gmail.com",
+  description: "A 36 hour hackathon in UC Riverside",
+  length: 36,
+  date: new Date("2025-04-19T08:00:00"),
+  end: new Date("2025-04-20T20:00:00"),
   packet: "",
   devpost: "https://devpost.com/",
-  domain: "https://www.placeholder.com",
-  instagram: "https://www.instagram.com",
-  linkedin: "https://www.linkedin.com",
+  domain: "https://www.citrushack.com/",
+  instagram: "https://www.instagram.com/citrushack_ucr/",
+  linkedin: "https://www.linkedin.com/company/cutie-hack/",
   discord: "https://www.discord.com",
-  heart: "🤎",
+  heart: "🧡",
 };
 
 export default data;
