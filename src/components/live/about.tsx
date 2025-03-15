811cd33fdd533787d1a03c1bc206c993010c24bc
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-03-14 18:14:42 -0700

    taaha

--- src/components/live/about.tsx
+++ src/components/live/about.tsx
@@ -6,6 +6,7 @@ import board from "@/public/assets/board.svg";
 import boxes from "@/public/assets/boxes.svg";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import taaha from "@/public/assets/taaha.png";
 import Card from "./about-card";
 
 const About = () => {
@@ -104,7 +105,7 @@ const About = () => {
               transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
               className="absolute right-[32%] top-2 w-[19%]"
             >
-              <Image src={film1} alt="film 1" />
+              <Image src={taaha} alt="film 1" />
             </motion.p>
             <Image src={camera1} alt="camera" className="w-[55%]" />
             <div className="absolute bottom-12 h-10 w-60 rounded bg-white opacity-70 blur-lg" />
