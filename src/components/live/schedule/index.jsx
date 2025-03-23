8fc7a4fc53012467a3126322a6aca72fdcca5aa7
seanquiambao <slquiambao03@gmail.com>
2025-03-22 21:23:32 -0700

    finished desktop frontend

--- src/components/live/schedule/index.jsx
+++ src/components/live/schedule/index.jsx
@@ -1,6 +1,6 @@
 import { api } from "@/utils/api";
 import Events from "./events";
-import Title from "@/components/ui/title";
+import TextStroke from "../text-stroke";
 
 const Schedule = async () => {
   const { items } = await api({
@@ -29,8 +29,30 @@ const Schedule = async () => {
 
   return (
     <div>
-      {" "}
-      <Title>Schedule</Title> <Events events={items} totalDays={totalDays} />
+      <div className="flex w-full flex-col items-center">
+        <div className="w-1/3 rounded-lg border-4 border-black bg-citrus-beige p-5 font-lily">
+          <TextStroke
+            title="Schedule"
+            color="text-outline-red text-center text-2xl text-citrus-yellow md:text-9xl"
+            subColor="text-center text-2xl text-citrus-yellow md:text-9xl"
+          />
+        </div>
+        <div className="p-4 font-kameron text-4xl text-white">
+          Pacific Standard Time (PST)
+        </div>
+
+        <div className="my-[5%] flex w-11/12 flex-col">
+          <span className="my-1 h-0.5 w-full bg-white" />
+          <span className="my-1 h-0.5 w-full bg-white" />
+          <span className="my-1 h-1 w-full bg-white" />
+        </div>
+      </div>
+      <Events events={items} totalDays={totalDays} />
+      <div className="my-[5%] flex w-11/12 flex-col">
+        <span className="my-1 h-0.5 w-full bg-white" />
+        <span className="my-1 h-0.5 w-full bg-white" />
+        <span className="my-1 h-1 w-full bg-white" />
+      </div>
     </div>
   );
 };
