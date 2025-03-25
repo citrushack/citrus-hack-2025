e65ab718ec6aa7d1ab7b6fffc7a76ab08415e7c1
stormyy00 <jonathan.trujillo0432@gmail.com>
2025-03-24 18:08:26 -0700

    nice

--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -8,7 +8,7 @@ import Navigation from "./navigation";
 // import Schedule from "./schedule";
 // import Tracks from "./tracks";
 import Sponsors from "./sponsors";
-// import Team from "./team/team";
+import Team from "./team/team";
 // import Committees from "./committees";
 // import Judges from "./judges";
 import FAQ from "./faq";
@@ -32,11 +32,11 @@ const Live = () => {
             <Navigation />
             <Landing complete />
             <About />
+            {/* <Tracks /> */}
             <Keynote />
             {/* <Schedule /> */}
             <Sponsors />
-            {/* <Team /> */}
-            {/* <Tracks /> */}
+            <Team />
             {/* 
     <Committees />
     <Judges />
