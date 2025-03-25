6c198286ae1cfb773733b36a8689adf8345bcad5
Shaodw709 <amirvs9675@gmail.com>
2025-03-24 19:01:49 -0700

    com

--- src/components/live/committees.tsx
+++ src/components/live/committees.tsx
@@ -1,9 +1,23 @@
 import Title from "@/components/ui/title";
+import ITEMS from "@/data/live/committees";
 
 const Committees = () => {
   return (
-    <div className="flex items-center justify-center">
-      <Title>Committees</Title>
+    <div className="justify-items-center space-y-10">
+      <Title>COMMITTEES</Title>
+
+      <div className="flex font-bold text-white sm:text-xl lg:text-3xl">
+        {ITEMS.map(({ name, members }) => (
+          <div key={name} className="px-5">
+            {name}
+            <div className="grid grid-rows-4 justify-items-center sm:text-xs md:text-xl lg:text-xl">
+              {members.map((member, index) => (
+                <span key={index}>{member.name}</span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/data/live/committees.tsx
@@ -0,0 +1,39 @@
+const ITEMS = [
+  {
+    name: "OPERATIONS",
+    members: [
+      { name: "Billy" },
+      { name: "Bob" },
+      { name: "Billy" },
+      { name: "Bob" },
+    ],
+  },
+  {
+    name: "SPONSORSHIP",
+    members: [
+      { name: "Billy" },
+      { name: "Bob" },
+      { name: "Billy" },
+      { name: "Bob" },
+    ],
+  },
+  {
+    name: "UI/UX",
+    members: [
+      { name: "Billy" },
+      { name: "Jimmy" },
+      { name: "Billy" },
+      { name: "Bob" },
+    ],
+  },
+  {
+    name: "SOFTWARE",
+    members: [
+      { name: "Billy" },
+      { name: "Bob" },
+      { name: "Billy" },
+      { name: "Bob" },
+    ],
+  },
+];
+export default ITEMS;
