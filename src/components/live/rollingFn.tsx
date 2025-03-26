baebd6d92dbe084df9ccd2cc51b8b717cc7bd8f7
Shaodw709 <amirvs9675@gmail.com>
2025-03-26 15:05:03 -0700

    add rolling

---
+++ src/components/live/rollingFn.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+interface FilmProps {
+  imageSrc: string;
+  duration?: number;
+  count?: number;
+}
+
+export default function film({
+  imageSrc,
+  duration = 999,
+  count = 100,
+}: FilmProps) {
+  return (
+    <div className="relative w-full overflow-hidden max-sm:hidden">
+      <motion.div
+        className="flex w-max"
+        initial={{ x: "0%" }}
+        animate={{ x: "-100%" }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "loop",
+          duration,
+          ease: "linear",
+        }}
+      >
+        {Array.from({ length: count }).map((_, index) => (
+          <div key={index} className="shrink-0">
+            <Image src={imageSrc} alt="nice" className="my-16" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
+}
