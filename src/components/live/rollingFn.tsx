fd208fa17f8932d2ef1a63cc8ce9185f9987cebd
Shaodw709 <amirvs9675@gmail.com>
2025-03-29 16:46:06 -0700

    fixed maybe

--- src/components/live/rollingFn.tsx
+++ src/components/live/rollingFn.tsx
@@ -1,38 +1,20 @@
-"use client";
-
-import { motion } from "framer-motion";
 import Image from "next/image";
 
 interface FilmProps {
   imageSrc: string;
-  duration?: number;
   count?: number;
 }
 
-export default function film({
-  imageSrc,
-  duration = Infinity,
-  count = 100,
-}: FilmProps) {
+export default function film({ imageSrc, count = 100 }: FilmProps) {
   return (
-    <div className="relative w-full overflow-hidden">
-      <motion.div
-        className="flex w-max"
-        initial={{ x: "0%" }}
-        animate={{ x: "-100%" }}
-        transition={{
-          repeat: Infinity,
-          repeatType: "loop",
-          duration,
-          ease: "linear",
-        }}
-      >
+    <div className="relative my-16 w-full overflow-hidden">
+      <div className="flex w-max">
         {Array.from({ length: count }).map((_, index) => (
           <div key={index} className="shrink-0">
             <Image src={imageSrc} alt="nice" className="my-16" />
           </div>
         ))}
-      </motion.div>
+      </div>
     </div>
   );
 }
