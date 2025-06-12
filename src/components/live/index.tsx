a57ed208ccd563b7d95ee0ab18dbb36e38846057
allison-pham <allisonppham@gmail.com>
2025-06-12 15:17:36 -0700

    Fix

--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -6,17 +6,17 @@ import About from "./about";
 import Navigation from "./navigation";
 // import Cursor from "./custom/cursor";
 // import Schedule from "./schedule";
-import Tracks from "./tracks";
-import Sponsors from "./sponsors";
+// import Tracks from "./tracks";
+// import Sponsors from "./sponsors";
 // import Team from "./team/team";
-import Committees from "./committees";
-import Judges from "./judges";
+// import Committees from "./committees";
+// import Judges from "./judges";
 import FAQ from "./faq";
 import Footer from "./footer";
 // import CurtainReveal from "./reveal";
-import Schedule from "./schedule";
-import Team from "./team/team";
-import Keynote from "./keynote";
+// import Schedule from "./schedule";
+// import Team from "./team/team";
+// import Keynote from "./keynote";
 
 const Live = () => {
   // const [complete, setComplete] = useState(false);
